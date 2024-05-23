0a34bbbf102f5d6519ca1a354ccc7c1302f1bac1
Daniela Domingues <dani.domingues10@outlook.com>
2024-05-22 22:30:46 -0300

    adicionar dropdown mes

--- app.py
+++ app.py
@@ -6,9 +6,17 @@ import numpy as np
 
 app = Dash(__name__)
 
-# Definição dos dados do mapa de calor
-heatmap_data = [
-    [00, 40, 30, 60, 24, 100, 99],
+# Definição dos dados do mapa de calor para abril e maio
+heatmap_data_abril = [
+    [10, 20, 30, 40, 50, 60, 70],
+    [15, 25, 35, 45, 55, 65, 75],
+    [20, 30, 40, 50, 60, 70, 80],
+    [25, 35, 45, 55, 65, 75, 85],
+    [30, 40, 50, 60, 70, 80, 90],
+]
+
+heatmap_data_maio = [
+    [0, 40, 30, 60, 24, 100, 99],
     [10, 45, 50, 25, 70, 97, 100],
     [15, 40, 32, 60, 99, 100, 98],
     [17, 37, 30, 60, 24, 95, 100],
@@ -24,29 +32,36 @@ days_numbers = [
     [29, 30, 31]
 ]
 
-# Criação do objeto Heatmap com os dados e configurações
-fig_heatmap = go.Figure(data=go.Heatmap(z=heatmap_data, text=days_numbers, colorscale='blues'))
-fig_heatmap.update_layout(
-    title={
-        'text': "Maio",
-        'x': 0.5,  # Define a posição horizontal do título
-        'xanchor': 'center',  # Ancora o título ao centro horizontal
-        'yanchor': 'top'  # Ancora o título ao topo vertical
-    },
-    xaxis=dict(
-        showgrid=True, 
-        gridwidth=1, 
-        gridcolor='rgba(0,0,0,0.1)',  # Mostra as linhas verticais entre os dias
-        showticklabels=False  # Remove os números de legenda do eixo x
-    ),
-    yaxis=dict(
-        showgrid=True, 
-        gridwidth=1, 
-        gridcolor='rgba(0,0,0,0.1)',  # Mostra as linhas horizontais entre os dias
-        showticklabels=False  # Remove os números de legenda do eixo y
+# Função para criar o gráfico de calor
+def create_heatmap(month):
+    if month == 'Abril':
+        heatmap_data = heatmap_data_abril
+    else:
+        heatmap_data = heatmap_data_maio
+    
+    fig = go.Figure(data=go.Heatmap(z=heatmap_data, text=days_numbers, colorscale='blues'))
+    fig.update_layout(
+        title={
+            'text': month,
+            'x': 0.5,  # Define a posição horizontal do título
+            'xanchor': 'center',  # Ancora o título ao centro horizontal
+            'yanchor': 'top'  # Ancora o título ao topo vertical
+        },
+        xaxis=dict(
+            showgrid=True, 
+            gridwidth=1, 
+            gridcolor='rgba(0,0,0,0.1)',  # Mostra as linhas verticais entre os dias
+            showticklabels=False  # Remove os números de legenda do eixo x
+        ),
+        yaxis=dict(
+            showgrid=True, 
+            gridwidth=1, 
+            gridcolor='rgba(0,0,0,0.1)',  # Mostra as linhas horizontais entre os dias
+            showticklabels=False  # Remove os números de legenda do eixo y
+        )
     )
-)
-fig_heatmap.update_traces(textfont=dict(color='black'))
+    fig.update_traces(textfont=dict(color='black'))
+    return fig
 
 # Definindo os dados do gráfico de linha
 horas_do_dia = np.arange(6, 24)  # Horas de funcionamento: 6h às 23h
@@ -94,7 +109,29 @@ app.layout = html.Div([
                 html.Label("Funcionários Ativos:", className="body-do-painel-texto"),
                 html.H2("50", className="body-do-painel-texto")  # Quantidade de funcionários ativos
             ]),
- 
+            html.Div(className="painel-lateral", style={'marginTop': '20px'}, children=[
+                html.Label("Selecione o Mês:", className="dropmes"),
+                dcc.Dropdown(
+                    id='month-dropdown',
+                    options=[
+                        {'label': 'Janeiro', 'value': 'Janeiro'},
+                        {'label': 'Fevereiro', 'value': 'Fevereiro'},
+                        {'label': 'Março', 'value': 'Março'},
+                        {'label': 'Abril', 'value': 'Abril'},
+                        {'label': 'Maio', 'value': 'Maio'},
+                        {'label': 'Junho', 'value': 'Junho'},
+                        {'label': 'Julho', 'value': 'Julho'},
+                        {'label': 'Agosto', 'value': 'Agosto'},
+                        {'label': 'Setembro', 'value': 'Setembro'},
+                        {'label': 'Outubro', 'value': 'Outubro'},
+                        {'label': 'Novembro', 'value': 'Novembro'},
+                        {'label': 'Dezembro', 'value': 'Dezembro'},
+                    ],
+                    value='Maio',
+                    clearable=False,
+                    className='lateral-dropdown'
+                ),
+            ]),
         ]),
         html.Div(className="contador", children=[
             html.Label("PESSOAS", style={'marginBottom': '10px'}),
@@ -110,18 +147,26 @@ app.layout = html.Div([
     ]),
     html.Div(className="calendario", children=[
         html.Label("CALENDARIO"),
-        dcc.Graph(figure=fig_heatmap)
+        dcc.Graph(id='heatmap', figure=create_heatmap('Maio'))
     ]),
     html.Div(className="grafico-de-linha", children=[
         dcc.Graph(figure=fig_linha)
     ]),
     html.Div(className="nao-definido", children=[
-    html.Label("PODUTOS MAIS VENDIDOS:", className="body-do-painel-texto"),
-    html.Ul([
-    html.Li(produto) for produto in produtos_mais_vendidos
-                ])
-            ])
+        html.Label("PRODUTOS MAIS VENDIDOS:", className="body-do-painel-texto"),
+        html.Ul([
+            html.Li(produto) for produto in produtos_mais_vendidos
+        ])
+    ])
 ])
+
+# Callback para atualizar o gráfico de calor com base na seleção do mês
+@app.callback(
+    Output('heatmap', 'figure'),
+    Input('month-dropdown', 'value')
+)
+def update_heatmap(selected_month):
+    return create_heatmap(selected_month)
 
 # Função de callback para atualizar o contador
 @app.callback(Output('contador-texto', 'children'), [Input('interval-component', 'n_intervals')])
